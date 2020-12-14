3a47ba4ef2c89b7de961e47dce89bb7b4aea306c
松本一将 <k.matsumoto@benjmain.co.jp>
2020-12-14 15:02:32 +0900

    solve-prop-drilling-issue

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,9 +2,9 @@ import React, { useContext } from 'react'
 import Event from './Event'
 import AppContext from '../contexts/AppContext'
 
-const Events = ({ state, dispatch }) => {
+const Events = () => {
   // Providerからデータを受け取るuseContext
-  const value = useContext(AppContext)
+  const { state } = useContext(AppContext)
   return (
     <React.Fragment>
       <h4>イベント一覧</h4>
@@ -18,7 +18,7 @@ const Events = ({ state, dispatch }) => {
           </tr>
         </thead>
         <tbody>
-          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
+          { state.map((event, index) => (<Event key={index} event={event} />))}
         </tbody>
       </table>
     </React.Fragment>
